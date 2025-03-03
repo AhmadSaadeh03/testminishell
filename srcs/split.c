65a9f5db32b69eee5bb059b6a8de03e972b0939d
Ahmad Saadeh <asaadeh@c1r5s12.42amman.com>
2025-03-03 15:57:33 +0300

    ahmad

--- srcs/split.c
+++ srcs/split.c
@@ -6,7 +6,7 @@
 /*   By: asaadeh <asaadeh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 13:42:38 by asaadeh           #+#    #+#             */
-/*   Updated: 2025/03/01 16:55:35 by asaadeh          ###   ########.fr       */
+/*   Updated: 2025/03/03 15:44:45 by asaadeh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 int split_space(t_minishell *shell)
 {
-    shell->token_space = &shell->name;
     shell->token_space = ft_split(shell->name, ' ');
     if (!shell->token_space) {
         printf("Error: Failed to split the input\n");
@@ -22,38 +21,37 @@ int split_space(t_minishell *shell)
     }
     return 0;   
 }
-
 int split_operation(t_minishell *shell, char operator)
 {
     char *temp;
     int i = 0, len, new_len;
+    int inside_quotes = 0;  // Flag to track if we are inside quotes
     temp = ft_strdup(shell->name);
     if (!temp)
         return 1;
     len = ft_strlen(temp);
+
     while (i < len)
     {
-        if (temp[i] == operator)
+        if (temp[i] == operator && operator != '"')
         {
-            if (i == 0 || i == len - 1) 
+            if ((i == 0 || i == len - 1) && (operator == '|' || operator == '>' || operator == '<')) 
             {
-                printf("%s\n", "syntax error near unexpected token");
+                printf("syntax error near unexpected token\n");
                 free(temp);
                 return 1;
             }
             if (i > 0 && temp[i - 1] != ' ') 
             {
-                // Create a new string with extra space for the new space
                 new_len = len + 1; // Space before the operator
                 char *new_temp = malloc(new_len + 1);
                 if (!new_temp)
                 {
                     free(temp);
-                    return 1; // Memory allocation error
+                    return 1;
                 }
-                // Copy the part before the operator and the space
                 ft_memcpy(new_temp, temp, i);
-                new_temp[i] = ' '; // Add space
+                new_temp[i] = ' ';
                 ft_memcpy(new_temp + i + 1, temp + i, len - i + 1);
                 free(temp);
                 temp = new_temp;
@@ -63,14 +61,13 @@ int split_operation(t_minishell *shell, char operator)
             // Add space after operator if not present
             if (i + 1 < len && temp[i + 1] != ' ') 
             {
-                new_len = len + 1; // Space after the operator
+                new_len = len + 1;
                 char *new_temp = malloc(new_len + 1);
                 if (!new_temp)
                 {
                     free(temp);
-                    return 1; // Memory allocation error
+                    return 1;
                 }
-                // Copy the part before the operator, add space, and then the rest
                 ft_memcpy(new_temp, temp, i + 1);
                 new_temp[i + 1] = ' ';
                 ft_memcpy(new_temp + i + 2, temp + i + 1, len - i);
@@ -79,8 +76,58 @@ int split_operation(t_minishell *shell, char operator)
                 len = new_len;
             }
         }
+        
+        // Handle quotes and treat everything inside as one token
+        if (temp[i] == '"' && operator == '"')
+        {
+            if (!inside_quotes)
+            {
+                // If we encounter the first quote, check if a space is needed before
+                if (i > 0 && temp[i - 1] != ' ') 
+                {
+                    new_len = len + 1;
+                    char *new_temp = malloc(new_len + 1);
+                    if (!new_temp)
+                    {
+                        free(temp);
+                        return 1;
+                    }
+                    ft_memcpy(new_temp, temp, i);
+                    new_temp[i] = ' ';  // Add a space before the first quote
+                    ft_memcpy(new_temp + i + 1, temp + i, len - i + 1);
+                    free(temp);
+                    temp = new_temp;
+                    len = new_len;
+                    i++; // Move index forward due to the inserted space
+                }
+                inside_quotes = 1;  // Flag set to true as we are inside quotes
+            }
+            else
+            {
+                // If we encounter the closing quote, check if space is needed after
+                if (i + 1 < len && temp[i + 1] != ' ') 
+                {
+                    new_len = len + 1;
+                    char *new_temp = malloc(new_len + 1);
+                    if (!new_temp)
+                    {
+                        free(temp);
+                        return 1;
+                    }
+                    ft_memcpy(new_temp, temp, i + 1);
+                    new_temp[i + 1] = ' ';  // Add a space after the closing quote
+                    ft_memcpy(new_temp + i + 2, temp + i + 1, len - i);
+                    free(temp);
+                    temp = new_temp;
+                    len = new_len;
+                }
+                inside_quotes = 0;  // Reset flag as we are outside quotes now
+            }
+        }
+        
         i++;  // Move to the next character
     }
+
     free(shell->name);
     shell->name = temp;
     return 0;
@@ -90,30 +137,19 @@ int split(t_minishell *shell)
     int i = 0;
     if (!*shell->name || !shell->name)
     {
-        return 1;
+        return 0;
     }
     while (shell->name[i])
     {
-        if (shell->name[i] == '|' || shell->name[i] == '<' ||shell->name[i] == '>'|| shell->name[i] == '-')
+        if (shell->name[i] == '|' || shell->name[i] == '<' || shell->name[i] == '>' || shell->name[i] == '"')
         {
             if (split_operation(shell, shell->name[i]) == 1)
                 return 1;
         }
-        if (shell->name[i] == 34)
-        {
-            if (!closed_quotes(shell, shell->name[i])) // check if the qoutes is closed the function is in checks.c if it returns 1 this means it's closed else is error
-            {
-                printf("%s\n", "Invalid quotes");
-                return 1;
-            }
-           qoutes_handling(shell,shell->name[i]);
-        }    
         i++;
     }
-    //split_space(shell);
     if (split_space(shell) == 1)
         return 1;
-    
     t_node *head = create_node_list(shell->token_space);
     if (head)
     {
