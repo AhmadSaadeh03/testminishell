2881ec4337361e0e27cc688bc3c2986d53063954
Fatima Ghanem <fghanem@c2r3s3.42amman.com>
2025-04-26 16:31:24 +0300

    seg fixing , ft_echo

--- srcs/expand/expand_helpers.c
+++ srcs/expand/expand_helpers.c
@@ -6,7 +6,7 @@
 /*   By: fghanem <fghanem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 16:27:25 by fghanem           #+#    #+#             */
-/*   Updated: 2025/04/26 11:45:53 by fghanem          ###   ########.fr       */
+/*   Updated: 2025/04/26 16:16:44 by fghanem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,43 +74,32 @@ void	my_setenv(t_env **env_list, char *name, char *value)
 	t_env	*new_node;
 
 	temp = *env_list;
-	// Check if the variable already exists
 	while (temp)
 	{
 		if (ft_strcmp(temp->env_name, name) == 0)
 		{
-			printf("Updating existing variable: %s:  %s\n", name, value);
 			free(temp->value);
 			temp->value = ft_strdup(value);
 			return ;
 		}
 		temp = temp->next;
 	}
-	printf("Adding new variable: %s::    %s\n", name, value);
 	new_node = malloc(sizeof(t_env));
 	if (!new_node)
-	{
-		printf("Memory allocation failed for new_node\n");
 		return ;
-	}
 	new_node->env_name = ft_strdup(name);
-	printf("name : %s\n", new_node->env_name);
 	if (value)
 	{
 		new_node->value = ft_strdup(value);
 		if (!new_node->value)
 		{
-			perror("adding error");
 			free(new_node->env_name);
 			free(new_node);
 			return ;
 		}
-		printf("vall : %s\n", new_node->value);
 	}
 	else
 		new_node->value = NULL;
 	new_node->next = *env_list;
 	*env_list = new_node;
-	printf("name : %s\n", (*env_list)->env_name);
-	printf("val : %s\n", (*env_list)->value);
 }
